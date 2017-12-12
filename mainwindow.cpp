5968533b6c3689da751265a8c9846637c073e392
NearSGB <1844376591@qq.com>
2017-12-12 16:24:25 +0800

    use qtimer and qlabel fix 4 cameras image into the same ui with the buttons

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,11 +7,33 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    connect(&timer, SIGNAL(timeout()), this, SLOT(onTimeout()));
 }
 
 MainWindow::~MainWindow()
 {
+    for(int i = 0; i < camera_num; i++)
+        cap[i].release();
     delete ui;
+}
+
+void MainWindow::onTimeout(){
+    for(int i = 0; i < camera_num; i++)
+        if(cap[i].isOpened()){
+            cap[i]>>frame_temp;
+            cv::resize(frame_temp, frame[i], Size(camera_w, camera_h));
+        }
+
+    hconcat(frame[0], frame[1], combine1);
+    hconcat(frame[2], frame[3], combine2);
+    vconcat(combine1, combine2, combineAll);
+
+
+    QSize qs = ui->label_video->rect().size();
+    ui->label_video->setScaledContents(true);
+    QImage image = MatToQImage(combineAll);
+    ui->label_video->setPixmap(QPixmap::fromImage(image));
+    ui->label_video->repaint();
 }
 
 
@@ -19,44 +41,24 @@ void MainWindow::on_pushButton_clicked()
 {
     isRun = true;
     ui->textEdit->setTextColor(QColor("green"));
-    ui->textEdit->setPlainText("run");
-    const int camera_num = 4;
-    const int camera_w = 800, camera_h = 450;
-    VideoCapture cap[camera_num];
-    Mat frame[camera_num], frame_temp;
-    Mat combine1, combine2, combineAll;
-    string windowname = "video";
-    namedWindow(windowname, WINDOW_NORMAL);
-    moveWindow(windowname,300, 0);
+    ui->textEdit->setPlainText("  run");
+
     for(int i = 0; i < camera_num; i++){
         cap[i].open(i);
         frame[i] = Mat(Size(camera_w, camera_h), CV_8UC3, Scalar(0,0,0));
     }
-    while(isRun)
-    {
-        for(int i = 0; i < camera_num; i++)
-            if(cap[i].isOpened()){
-                cap[i]>>frame_temp;
-                cv::resize(frame_temp, frame[i], Size(camera_w, camera_h));
-            }
 
-        hconcat(frame[0], frame[1], combine1);
-        hconcat(frame[2], frame[3], combine2);
-        vconcat(combine1, combine2, combineAll);
-        imshow(windowname, combineAll);
-        waitKey(500);
-    }
-    for(int i = 0; i < camera_num; i++)
-        cap[i].release();
-    destroyAllWindows();
+    timer.start(333);
+
 }
 
 
 void MainWindow::on_pushButton_2_clicked()
 {
     isRun = false;
+    timer.stop();
     ui->textEdit->setTextColor(QColor("red"));
-    ui->textEdit->setPlainText("stop");
+    ui->textEdit->setPlainText("  stop");
 }
 
 
@@ -69,4 +71,41 @@ void MainWindow::on_exit_clicked()
 void MainWindow::on_pushButton_3_clicked()
 {
 
+}
+QImage MainWindow::MatToQImage(const cv::Mat& mat)
+{
+    // 8-bits unsigned, NO. OF CHANNELS = 1
+//    if (mat.type() == CV_8UC1)
+//    {
+//        QImage image(mat.cols, mat.rows, QImage::Format_Indexed8);
+//        // Set the color table (used to translate colour indexes to qRgb values)
+//        image.setColorCount(256);
+//        for (int i = 0; i < 256; i++)
+//        {
+//            image.setColor(i, qRgb(i, i, i));
+//        }
+//        // Copy input Mat
+//        uchar *pSrc = mat.data;
+//        for (int row = 0; row < mat.rows; row++)
+//        {
+//            uchar *pDest = image.scanLine(row);
+//            memcpy(pDest, pSrc, mat.cols);
+//            pSrc += mat.step;
+//        }
+//        return image;
+//    }
+//    // 8-bits unsigned, NO. OF CHANNELS = 3
+//    else
+    if (mat.type() == CV_8UC3)
+    {
+        // Copy input Mat
+        const uchar *pSrc = (const uchar*)mat.data;
+        // Create QImage with same dimensions as input Mat
+        QImage image(pSrc, mat.cols, mat.rows, mat.step, QImage::Format_RGB888);
+        return image.rgbSwapped();
+    }
+    else
+    {
+        return QImage();
+    }
 }
--- mainwindow.h
+++ mainwindow.h
@@ -4,7 +4,7 @@
 #include <QMainWindow>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
-#include <opencv2/videoio.hpp> // for camera
+#include <opencv2/videoio.hpp>
 #include "opencv2/opencv.hpp"
 #include "opencv2/imgproc.hpp"
 #include <QtWidgets/QMainWindow>
@@ -15,8 +15,13 @@
 #include <QLabel>
 #include <QImage>
 using namespace cv;
+
+constexpr int camera_num = 4;
+constexpr int camera_w = 800, camera_h = 450;
+
 namespace Ui {
 class MainWindow;
+
 }
 
 class MainWindow : public QMainWindow
@@ -26,10 +31,10 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
-
+    QImage  MatToQImage(const cv::Mat& mat);
 private slots:
     void on_pushButton_clicked();
-
+    void onTimeout();
     void on_pushButton_2_clicked();
 
     void on_exit_clicked();
@@ -37,9 +42,13 @@ private slots:
     void on_pushButton_3_clicked();
 
 private:
-    Ui::MainWindow *ui;
 
+    Ui::MainWindow *ui;
+    VideoCapture cap[camera_num]; //用来读取视频结构
+    QTimer timer;//视频播放的定时器
     bool isRun = true;
+    Mat frame[camera_num];
+    Mat frame_temp, combine1, combine2, combineAll;
 };
 
 #endif // MAINWINDOW_H
